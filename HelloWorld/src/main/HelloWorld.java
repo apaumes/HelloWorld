82e31632d15769add45715309bcc4376814107f7
apaumes <anni.paumees@itcollege.ee>
2016-11-08 21:30:58 +0200

    HelloWorld programm
    HelloWorld programmi lisamine

---
+++ HelloWorld/src/main/HelloWorld.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by Anni on 6.11.2016.
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
